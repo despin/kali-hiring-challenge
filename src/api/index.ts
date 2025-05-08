11c102a1ff05b63ef894e69876e296fb5d123e71
despin <sirdespin@gmail.com>
2025-05-08 13:01:38 -0300

    Removed old token

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,11 @@
 const API_BASE_URL = 'https://api.themoviedb.org/3';
+const TOKEN = 'YOUR_JWT_TOKEN_HERE'
 
 export default async function requestMovieDb(url: string) {
   const response = await fetch(`${API_BASE_URL}${url}`, {
     headers: new Headers({
       Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NTBhYmFhZWMyMDA0ZGUwZmFhOTA0YTdhYWVmMDI0MyIsInN1YiI6IjY0MGI4ZDI1NzI0ZGUxMDBjMTAyZWViMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.yiqfAHGnrs1yEwKWCn7HJmwnXwxcZzA1KPfdu1J3k68',
+        `Bearer ${TOKEN}`,
       'Content-Type': 'application/json',
       charset: 'utf8',
     }),
